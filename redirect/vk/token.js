5544eb17c16587418db01c2c560a02c9cf74fda8
Animenosekai <maxence.osawa@gmail.com>
2020-04-23 23:27:49 +0200

    giving up on changing the <p>

--- redirect/vk/token.js
+++ redirect/vk/token.js
@@ -12,11 +12,9 @@ const askedDate = new Date()
 window.localStorage.setItem('vkAPIToken_tokenCreationDate', askedDate)
 
 if(window.localStorage.getItem('vkAPIToken_accessToken') !== null){
-    document.getElementById('accessTokenStatus').innerHTML = "Redirecting to the sources check page..."
     console.log('Successfull!')
     console.log("Redirecting...")
     window.open('/devtools/sources-check', '_self')
 }else{
-    document.getElementById('accessTokenStatus').innerHTML = "There was an error while getting the token"
     console.error("There was an error while getting the token!")
 }
